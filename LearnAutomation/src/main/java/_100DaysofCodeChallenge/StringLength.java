7da0983032d880a4e7f666128f5c0cba6d87ddd7
LENOVO <LENOVO@Vinoth-DESKTOP-7RPL585>
2023-08-19 14:28:59 +0530

    Finding Length of a String

---
+++ LearnAutomation/src/main/java/_100DaysofCodeChallenge/StringLength.java
@@ -0,0 +1,14 @@
+package _100DaysofCodeChallenge;
+
+//Program to Find the Length of a String
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a = " JAVA ";
+		System.out.println("The Length of the String is " + a.length());
+	}
+
+}
