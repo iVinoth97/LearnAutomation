06c07ef7c232f541efeb954f57b7eee469bbae42
LENOVO <LENOVO@Vinoth-DESKTOP-7RPL585>
2023-08-19 14:23:52 +0530

    Simple String Reverse Program

---
+++ LearnAutomation/src/main/java/_100DaysofCodeChallenge/StringReverse.java
@@ -0,0 +1,13 @@
+package _100DaysofCodeChallenge;
+
+public class StringReverse {
+	
+ public static void main(String[] args) {
+	
+	 String a = "Java programming";
+	 StringBuilder b = new StringBuilder();
+	 b.append(a);
+	 b.reverse();
+	 System.out.println(b);
+}
+}
