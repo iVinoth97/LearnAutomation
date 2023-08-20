13870bf33c7de4816afd18fcfa50b908d8b4d7fd
LENOVO <LENOVO@Vinoth-DESKTOP-7RPL585>
2023-08-20 17:19:48 +0530

    to be completed

---
+++ LearnAutomation/src/main/java/_100DaysofCodeChallenge/SmallestNumberInArray.java
@@ -0,0 +1,20 @@
+package _100DaysofCodeChallenge;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SmallestNumberInArray {
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number of elements: ");
+		int n = scanner.nextInt();
+		int[] array = new int[n];
+		System.out.println("Enter the elements of the array: ");
+		for (int i = 0; i < n; i++) {
+		array[i] = scanner.nextInt();
+		
+		
+		}
+	}
+}
